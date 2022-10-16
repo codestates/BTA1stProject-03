822b1b25c44c393d47cc65dd2cc9bf8197d4fcfb
codeMonkey-shin <sdw0711@hanmail.net>
2022-10-16 17:11:30 +0900

    add : wallet utils

---
+++ extension-wallet/src/pages/component/wallet.js
@@ -0,0 +1,15 @@
+import {getOfflineSignerProto} from 'cosmjs-utils'
+import {chains} from 'chain-registry'
+import {FEES, osmosis} from 'osmojs'
+const config = require('../../config/config.json');
+const endPointUrl = config.endPoint.mainNet;
+
+
+
+const utils = {
+
+
+}
+
+
+module.exports = utils;
