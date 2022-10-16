dcf2bda7f7f3e240e5f791e9762488dd571467a1
codeMonkey-shin <sdw0711@hanmail.net>
2022-10-16 17:12:33 +0900

    feat : getAddress from mnemonic

--- extension-wallet/src/pages/component/wallet.js
+++ extension-wallet/src/pages/component/wallet.js
@@ -7,6 +7,16 @@ const endPointUrl = config.endPoint.mainNet;
 
 
 const utils = {
+    getAddress: async (mnemonic) => {  // 니모닉 코드를 사용해서 해당 지갑의 주소를 리턴
+        const chain = chains.find(({chain_name}) => chain_name === 'osmosis');
+        const signer = await getOfflineSignerProto({
+            mnemonic,
+            chain
+        });
+        let myAccount = await signer.getAccounts()
+        let myAddress = myAccount[0].address
+        return myAddress
+    },
 
 
 }
