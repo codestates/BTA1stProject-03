c612aa4e1bfa46011445280ef9cafed9f3f4a374
codeMonkey-shin <sdw0711@hanmail.net>
2022-10-16 17:19:43 +0900

    feat : getChain from ChainName

--- extension-wallet/src/pages/component/wallet.js
+++ extension-wallet/src/pages/component/wallet.js
@@ -1,12 +1,17 @@
 import {getOfflineSignerProto} from 'cosmjs-utils'
 import {chains} from 'chain-registry'
 import {FEES, osmosis} from 'osmojs'
+
 const config = require('../../config/config.json');
 const endPointUrl = config.endPoint.mainNet;
 
 
 
 const utils = {
+    getChain: async (chainName) => {  // 체인 이름을 토대로 체인 오브젝트 리턴
+        return chains.find(({chain_name}) => chain_name === chainName);
+    },
+
     getAddress: async (mnemonic) => {  // 니모닉 코드를 사용해서 해당 지갑의 주소를 리턴
         const chain = chains.find(({chain_name}) => chain_name === 'osmosis');
         const signer = await getOfflineSignerProto({
@@ -14,8 +19,7 @@ const utils = {
             chain
         });
         let myAccount = await signer.getAccounts()
-        let myAddress = myAccount[0].address
-        return myAddress
+        return myAccount[0].address
     },
     getBalance: async (address) => {  // 지갑의 주소를 사용해서 해당 지갑의 잔고를 리턴
         const {createRPCQueryClient} = osmosis.ClientFactory;
@@ -23,9 +27,13 @@ const utils = {
         return await client.cosmos.bank.v1beta1
             .allBalances({address: address})
     },
-    getFee: async (amount) => {  // low , medium, high 에 해당하는 값의 수수료를 리턴
+    getFee:  (amount) => {  // low , medium, high 에 해당하는 값의 수수료를 리턴
         return FEES.osmosis.swapExactAmountIn(amount)
     },
+    // send: async (fromAddress,toAddress,amount,mnemonic) => {  // 입력한 양의 코인을 송금한 후 해당 트랜잭션 정보 리턴
+    //     return null
+    // },
+
 
 
 }
